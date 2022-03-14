c5594cff98212e3266104bee068750f3f5f17cf7
Vaibhav Paliwal <15090114+vaibhavpali@users.noreply.github.com>
2022-03-14 17:14:59 +0530

    various widgets are implmented

--- lib/app_screens/RowColumn.dart
+++ lib/app_screens/RowColumn.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class RowColumn extends StatelessWidget {
@@ -8,7 +10,7 @@ class RowColumn extends StatelessWidget {
     return Center(
       child: Container(
         alignment: Alignment.center,
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.only(left: 10.0, top: 40.0),
         child: Column(
           children: [
             Row(
@@ -19,9 +21,9 @@ class RowColumn extends StatelessWidget {
                   textDirection: TextDirection.ltr,
                   style: TextStyle(
                       decoration: TextDecoration.none,
-                      fontSize: 40.0,
+                      fontSize: 35.0,
                       fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w300,
+                      fontWeight: FontWeight.w700,
                       color: Colors.white),
                 )),
                 Expanded(
@@ -30,9 +32,9 @@ class RowColumn extends StatelessWidget {
                   textDirection: TextDirection.ltr,
                   style: TextStyle(
                       decoration: TextDecoration.none,
-                      fontSize: 25.0,
+                      fontSize: 20.0,
                       fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w300,
+                      fontWeight: FontWeight.w700,
                       color: Colors.white),
                 ))
               ],
@@ -45,9 +47,9 @@ class RowColumn extends StatelessWidget {
                   textDirection: TextDirection.ltr,
                   style: TextStyle(
                       decoration: TextDecoration.none,
-                      fontSize: 40.0,
+                      fontSize: 35.0,
                       fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w300,
+                      fontWeight: FontWeight.w700,
                       color: Colors.white),
                 )),
                 Expanded(
@@ -56,17 +58,66 @@ class RowColumn extends StatelessWidget {
                   textDirection: TextDirection.ltr,
                   style: TextStyle(
                       decoration: TextDecoration.none,
-                      fontSize: 25.0,
+                      fontSize: 20.0,
                       fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w300,
+                      fontWeight: FontWeight.w700,
                       color: Colors.white),
                 ))
               ],
             ),
+            const ImageClass(),
+            const BookButton(),
           ],
         ),
         color: Colors.lightBlue,
       ),
     );
+  }
+}
+
+class ImageClass extends StatelessWidget {
+  const ImageClass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = const AssetImage('images/my_pic.png');
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
+    return Container(margin: const EdgeInsets.only(top: 20.0), child: image);
+  }
+}
+
+class BookButton extends StatelessWidget {
+  const BookButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250.0,
+      height: 50.0,
+      margin: const EdgeInsets.only(top: 30.0),
+      child: ElevatedButton(
+        onPressed: () => playSong(context),
+        child: const Text("Submit",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700,
+                color: Colors.white)),
+      ),
+    );
+  }
+
+  void playSong(BuildContext context) {
+    var alertDialog = const AlertDialog(
+      title: Text("Song played !!"),
+      content: Text("This is Track1"),
+    );
+
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
   }
 }
