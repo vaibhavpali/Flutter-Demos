5b961d8ff67b1ac108b8af2f76d37201eef722a2
Vaibhav Paliwal <15090114+vaibhavpali@users.noreply.github.com>
2022-03-15 18:03:59 +0530

    Dropdown button added.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
   ));
 }
 
+// ignore: use_key_in_widget_constructors
 class FavoriteCity extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -16,6 +17,8 @@ class FavoriteCity extends StatefulWidget {
 
 class _FavoriteCityState extends State<FavoriteCity> {
   String nameCity = "";
+  var _currencies = ['Rupees', 'AED', 'Pounds'];
+  var _currentItemSelected = 'Rupees';
 
   @override
   Widget build(BuildContext context) {
@@ -35,12 +38,35 @@ class _FavoriteCityState extends State<FavoriteCity> {
               },
             ),
             Padding(
-             padding: const EdgeInsets.all(40.0),
-             child: Text("Your best city is $nameCity"), 
+              padding: const EdgeInsets.all(40.0),
+              child: Text(
+                "Your best city is $nameCity",
+                style: const TextStyle(fontSize: 20.0),
+              ),
+            ),
+            DropdownButton<String>(
+              items: _currencies.map((String dropDownItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownItem,
+                  child: Text(dropDownItem),
+                );
+              }).toList(),
+              value: _currentItemSelected,
+              onChanged: (String? selectedItem) {
+                setState(() {
+                  _onDropDownItemSelected(selectedItem!);
+                });
+              },
             )
           ],
         ),
       ),
     );
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() {
+      _currentItemSelected = newValueSelected;
+    });
   }
 }
