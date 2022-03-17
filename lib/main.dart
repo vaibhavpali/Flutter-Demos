1eab1a9fcba5bc224fe642a124621336651a3a30
Vaibhav Paliwal <15090114+vaibhavpali@users.noreply.github.com>
2022-03-17 18:33:42 +0530

    Application for SI calculator is defined.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Stateful App Example",
+    debugShowCheckedModeBanner: false,
+    title: "Simple Interest Calculator",
     home: FavoriteCity(),
   ));
 }
@@ -16,50 +17,154 @@ class FavoriteCity extends StatefulWidget {
 }
 
 class _FavoriteCityState extends State<FavoriteCity> {
-  String nameCity = "";
-  var _currencies = ['Rupees', 'AED', 'Pounds'];
+  var _formKey = GlobalKey<FormState>();
+  final _currencies = ['Rupees', 'AED', 'Pounds'];
   var _currentItemSelected = 'Rupees';
+  final _minimumPadding = 5.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
+
+  TextEditingController principleController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+
+  var displayResult = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Stateful App Widget"),
+        title: const Text("Simple Interest Calculator"),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onChanged: (String inputValue) {
-                setState(() {
-                  nameCity = inputValue;
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Text(
-                "Your best city is $nameCity",
-                style: const TextStyle(fontSize: 20.0),
-              ),
-            ),
-            DropdownButton<String>(
-              items: _currencies.map((String dropDownItem) {
-                return DropdownMenuItem<String>(
-                  value: dropDownItem,
-                  child: Text(dropDownItem),
-                );
-              }).toList(),
-              value: _currentItemSelected,
-              onChanged: (String? selectedItem) {
-                setState(() {
-                  _onDropDownItemSelected(selectedItem!);
-                });
-              },
-            )
-          ],
-        ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+            padding: EdgeInsets.all(_minimumPadding * 4),
+            child: ListView(
+              children: <Widget>[
+                getMoneyImage(),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: principleController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter principle amount';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Principal',
+                        hintText: 'Enter principal, ex: 12000',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: roiController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Rate of interest';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Rate of interest',
+                        hintText: 'In percent',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: termController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please define period';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Term',
+                            hintText: 'Time in years',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0)),
+                          ),
+                        )),
+                        Container(
+                          width: _minimumPadding * 5,
+                        ),
+                        Expanded(
+                            child: DropdownButton<String>(
+                                items: _currencies.map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                value: _currentItemSelected,
+                                onChanged: (String? newValueSelected) {
+                                  _onDropDownItemSelected(newValueSelected!);
+                                }))
+                      ],
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: _minimumPadding, bottom: _minimumPadding),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text('Calculate', textScaleFactor: 1.5),
+                          onPressed: () {
+                            setState(() {
+                              if (_formKey.currentState!.validate()) {
+                                displayResult = _calculateReturns();
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text('Reset', textScaleFactor: 1.5),
+                          onPressed: () {
+                            setState(() {
+                              _reset();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(_minimumPadding * 2),
+                  child: Text(
+                    displayResult,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
@@ -68,5 +173,36 @@ class _FavoriteCityState extends State<FavoriteCity> {
     setState(() {
       _currentItemSelected = newValueSelected;
     });
+  }
+
+  Widget getMoneyImage() {
+    AssetImage assetImage = const AssetImage('images/money.png');
+    return Container(
+      child: Image(
+        image: assetImage,
+        width: 125.0,
+        height: 125.0,
+      ),
+      margin: EdgeInsets.all(_minimumPadding * 10),
+    );
+  }
+
+  String _calculateReturns() {
+    double principal = double.parse(principleController.text);
+    double roi = double.parse(roiController.text);
+    double term = double.parse(termController.text);
+    double totalAmountPayable = principal + (principal * roi * term) / 100;
+
+    String result =
+        'After $term years, your investment will be worth $totalAmountPayable $_currentItemSelected';
+    return result;
+  }
+
+  void _reset() {
+    principleController.text = '';
+    roiController.text = '';
+    termController.text = '';
+    displayResult = '';
+    _currentItemSelected = _currencies[0];
   }
 }
